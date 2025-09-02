6dc82789906edb33a0b7ca9c4bf9847e7bc8df19
Abdelrahman Qazzaz <arandombird671@gmail.com>
2025-09-02 21:50:56 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { test } from "./utils.js"; // always import with .js
+
+console.log("1");
+console.log(test());
+console.log("3");
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export function test() {
+  console.log("2"); // try to put a breakpoint here
+}
