7ca86a0dc30f19f1b167334528ad2d30b95dbac1
Abdelrahman Qazzaz <arandombird671@gmail.com>
2025-09-02 22:51:42 +0300

    http server
    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-import { test } from "./utils.js"; // always import with .js
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
-console.log("1");
-console.log(test());
-console.log("3");
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+const server = app.listen(3002, () => {
+  console.log("Server running on http://localhost:3002");
+  console.log("Debugger should remain attached");
+});
+// Keep the process alive
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception:", error);
+  server.close(() => {
+    process.exit(1);
+  });
+});
